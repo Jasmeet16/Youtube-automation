817ab7bd0246480821c54af9a8de43e71c0853d1
Jasmeet16 <66730339+Jasmeet16@users.noreply.github.com>
2020-06-20 15:22:01 +0530

    Create credent.js

---
+++ credent.js
@@ -0,0 +1,23 @@
+module.exports = [{
+   
+    "title" : "bambiha bole ",
+    "artist" : " amrit maan"
+}, 
+{
+    "title" : "so high",
+    "artist" : "sidu mooslewala"
+
+},
+{
+    "title" : "yalgaar",
+    "artist" : "carryminati"
+},
+{
+    "title" : "ajj kal ve",
+    "artist" : "barbie maan"
+},
+{
+    "title" : "trust me",
+    "artist" : "Gurnaam bhullar"
+}
+ ]
